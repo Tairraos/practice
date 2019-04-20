d46ff518b99c569ce8e37f339ff476d377270321
Xiaole Tao <xiaolet@cisco.com>
2019-04-20 23:38:24 +0800

    update readme

--- leetcode/tool/generateReadme.js
+++ leetcode/tool/generateReadme.js
@@ -1,35 +1,33 @@
 let x = require("xtool.js"),
+    oldData = require("./oldData"),
     path = require("path"),
-    list = [],
-    out = [],
-    pointer = 0;
+    maxId = +oldData.pop().questionId,
+    doneList = {},
+    mdContent = [],
+    pointer = 0,
+    mdLine = "",
+    questionId;
 
 x.readDir(path.resolve(__dirname, "..", "done"), {
     find: "*.js",
     recursive: true
-}).forEach(item => list[+item.replace(/.*(\d\d\d\d)\..*/, "$1")] = "done/" + item);
-out.push("### Leetcode 已完成题目列表");
-out.push("|      |`xxx0`|`xxx1`|`xxx2`|`xxx3`|`xxx4`|`xxx5`|`xxx6`|`xxx7`|`xxx8`|`xxx9`|");
-out.push("|------|------|------|------|------|------|------|------|------|------|------|");
+}).forEach(item => doneList[+item.replace(/.*(\d\d\d\d)\..*/, "$1")] = "done/" + item);
 
-var tmpid, tmpLine = "";
-while (pointer < list.length) {
-    tmpid = ("000" + pointer).slice(-4);
-    if (!(pointer % 10)) {
-        tmpLine = "|`" + tmpid.slice(0, 3) + "x`";
-    }
-    if (list[pointer]) {
-        tmpLine += "|[" + tmpid + "](" + list[pointer] + ")";
-    } else {
-        tmpLine += "| ";
-    }
+mdContent.push("### Leetcode 已完成题目列表");
+mdContent.push("|      |`xxx0`|`xxx1`|`xxx2`|`xxx3`|`xxx4`|`xxx5`|`xxx6`|`xxx7`|`xxx8`|`xxx9`|");
+mdContent.push("|------|------|------|------|------|------|------|------|------|------|------|");
+
+while (pointer < maxId) {
+    questionId = ("000" + pointer).slice(-4);
+    if (!(pointer % 10)) mdLine = "|`" + questionId.slice(0, 3) + "x`";
+    if (doneList[pointer]) mdLine += "|[" + questionId + "](" + doneList[pointer] + ")";
+    else mdLine += "|`" + questionId + "`";
     pointer += 1;
-    if (!(pointer % 10) || pointer === list.length) {
-        tmpLine += "|";
-        out.push(tmpLine);
+    if (!(pointer % 10) || pointer === doneList.length) {
+        mdLine += "|";
+        mdContent.push(mdLine);
     }
 }
 
-x.saveFile(path.resolve(__dirname, "..", "README.md"), out.join("\n"));
-
+x.saveFile(path.resolve(__dirname, "..", "README.md"), mdContent.join("\n"));
 console.log("已经生成新的Readme");
