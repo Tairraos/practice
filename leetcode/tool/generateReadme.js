7222ed7a4c1648184b5b38ef64941f5ff98277b7
Xiaole Tao <xiaolet@cisco.com>
2019-04-20 23:45:17 +0800

    update

--- leetcode/tool/generateReadme.js
+++ leetcode/tool/generateReadme.js
@@ -14,20 +14,20 @@ x.readDir(path.resolve(__dirname, "..", "done"), {
 }).forEach(item => doneList[+item.replace(/.*(\d\d\d\d)\..*/, "$1")] = "done/" + item);
 
 mdContent.push("### Leetcode 已完成题目列表");
-mdContent.push("|      |`xxx0`|`xxx1`|`xxx2`|`xxx3`|`xxx4`|`xxx5`|`xxx6`|`xxx7`|`xxx8`|`xxx9`|");
+mdContent.push("|      |`---0`|`---1`|`---2`|`---3`|`---4`|`---5`|`---6`|`---7`|`---8`|`---9`|");
 mdContent.push("|------|------|------|------|------|------|------|------|------|------|------|");
 
-while (pointer < maxId) {
+while (pointer <= maxId) {
     questionId = ("000" + pointer).slice(-4);
-    if (!(pointer % 10)) mdLine = "|`" + questionId.slice(0, 3) + "x`";
+    if (!(pointer % 10)) mdLine = "|`" + questionId.slice(0, 3) + "-`";
     if (doneList[pointer]) mdLine += "|[" + questionId + "](" + doneList[pointer] + ")";
-    else mdLine += "| . ";
+    else mdLine += "| ";
     pointer += 1;
-    if (!(pointer % 10) || pointer === doneList.length) {
+    if (!(pointer % 10) || pointer === maxId) {
         mdLine += "|";
         mdContent.push(mdLine);
     }
 }
 
 x.saveFile(path.resolve(__dirname, "..", "README.md"), mdContent.join("\n"));
-console.log("已经生成新的Readme");
+console.log("已经生成新的Readme, 最大题目：" + maxId);
