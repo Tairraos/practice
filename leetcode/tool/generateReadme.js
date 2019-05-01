c9b8cd0441cf26d0082d98f5a5d38e1e50ceb3e5
Xiaole Tao <xiaolet@cisco.com>
2019-05-01 22:03:12 +0800

    更新工具

--- leetcode/tool/generateReadme.js
+++ leetcode/tool/generateReadme.js
@@ -3,6 +3,7 @@ let x = require("xtool.js"),
     path = require("path"),
     maxId = +oldData.pop().questionId,
     doneList = {},
+    difficultyList = {},
     mdContent = [],
     pointer = 0,
     mdLine = "",
@@ -12,20 +13,22 @@ x.readDir(path.resolve(__dirname, "..", "done"), {
     find: "*.js",
     recursive: true
 }).forEach(item => doneList[+item.replace(/.*(\d\d\d\d)\..*/, "$1")] = "done/" + item);
+oldData.forEach(item => difficultyList[item.questionId] = item.difficulty);
 
 mdContent.push("### Leetcode 已完成题目列表");
-mdContent.push("|      |`---0`|`---1`|`---2`|`---3`|`---4`|`---5`|`---6`|`---7`|`---8`|`---9`|");
+mdContent.push("| | LE | ET | CO | DE | 已经 | 解答 | 完成 | 题目 | 索引 | 列表 |");
 mdContent.push("|------|------|------|------|------|------|------|------|------|------|------|");
 
 while (pointer <= maxId) {
     questionId = ("000" + pointer).slice(-4);
-    if (!(pointer % 10)) mdLine = "|`" + questionId.slice(0, 3) + "-`";
-    if (doneList[pointer]) mdLine += "|[" + questionId + "](" + doneList[pointer] + ")";
+    if (!(pointer % 10)) mdLine = "|`" + questionId.slice(0, 3) + "x`";
+    if (doneList[pointer]) mdLine += "|[" + questionId + "](" + doneList[pointer] + ")" + difficultyList[questionId];
     else mdLine += "| ";
     pointer += 1;
     if (!(pointer % 10) || pointer === maxId) {
         mdLine += "|";
-        mdContent.push(mdLine);
+        if (!mdLine.match(/x`(\| )*\|$/)) //不写入全空行
+            mdContent.push(mdLine);
     }
 }
 
