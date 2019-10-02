2b114f9f49f616bd011c090f9129ec607b7c28f7
Xiaole Tao <xiaolet@cisco.com>
2019-10-02 10:12:45 +0800

    update tool

--- leetcode/tool/progress.js
+++ leetcode/tool/progress.js
@@ -32,7 +32,7 @@ if (args[1] === "readme") {
     questionData.forEach(item => titleDict[item.questionId] = item);
 
     readme.push("### Leetcode Javascript 已完成题目列表");
-    readme.push(`完成度：${doneListJS.length}/${questionData.length-paid.length} (有${paid.length}题付费题未计算在总数内, 附[所有题目清单](LIST.md))`);
+    readme.push(`完成度：${doneListJS.length}/${questionData.length - paid.length} (有${paid.length}题付费题未计算在总数内, 附[所有题目清单](LIST.md))`);
     doneListJS.forEach(
         item => {
             let qid = item.replace(/.*(\d\d\d\d)\.[\w-]+\.js/, "$1"),
@@ -45,7 +45,7 @@ if (args[1] === "readme") {
     );
     readme.push("");
     readme.push("### Leetcode Python 已完成题目列表");
-    readme.push(`完成度：${doneListPY.length}/${questionData.length-paid.length} (有${paid.length}题付费题未计算在总数内, 附[所有题目清单](LIST.md))`);
+    readme.push(`完成度：${doneListPY.length}/${questionData.length - paid.length} (有${paid.length}题付费题未计算在总数内, 附[所有题目清单](LIST.md))`);
     doneListPY.forEach(
         item => {
             let qid = item.replace(/.*(\d\d\d\d)\.[\w-]+\.py/, "$1"),
@@ -60,17 +60,17 @@ if (args[1] === "readme") {
     x.saveFile(path.resolve(__dirname, "..", "README.md"), readme.join("\n"));
 
     list.push("### Leetcode 题目清单");
-    list.push(`总题数：${questionData.length} / 未付费题：${questionData.length-paid.length} / 付费题：${paid.length}`);
+    list.push(`总题数：${questionData.length} / 未付费题：${questionData.length - paid.length} / 付费题：${paid.length}`);
     questionData.forEach(
         item => {
             let remote = `https://leetcode-cn.com/problems/${item.titleSlug}/`,
                 difficulty = difficultyDict[item.level];
-            list.push(`- [${item.qid} - ${item.titleCn || item.titleEn}](${remote}) 难度：${difficulty}${item.isPaidOnly?" / 付费题":""}`);
+            list.push(`- [${item.questionId} - ${item.titleCn || item.titleEn}](${remote}) 难度：${difficulty}${item.isPaidOnly?" / 付费题":""}`);
         }
     );
     x.saveFile(path.resolve(__dirname, "..", "LIST.md"), list.join("\n"));
 
-    console.log(`已经生成新的Readme, 总题数: ${questionData.length-paid.length}, 已答: ${doneListJS.length}`);
+    console.log(`已经生成新的Readme, 总题数: ${questionData.length - paid.length}, 已答: ${doneListJS.length}`);
     process.exit();
 }
 
